a112ca130241cbb1b54d609879352a1bae07263c
jackpoz <giacomopoz@gmail.com>
2019-03-17 18:05:44 +0100

    Fix release-tdb.sh and adjust some commands

--- release-tdb.sh
+++ release-tdb.sh
@@ -47,22 +47,27 @@ cd check_install/bin
 cp ../etc/worldserver.conf.dist ../etc/worldserver.conf
 
 #  7. download latest TDB
-OLD_TDB=$(curl https://api.github.com/repos/TrinityCore/TrinityCore/releases | jq 'map(select(.tag_name|startswith("TDB335"))) | sort_by(.created_at) | reverse | .[0]')
+OLD_TDB_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/TrinityCore/TrinityCore/releases)
+OLD_TDB=$(echo $OLD_TDB_RESPONSE | jq 'map(select(.tag_name|startswith("TDB335"))) | sort_by(.created_at) | reverse | .[0]')
 OLD_TDB_VERSION=$(echo $OLD_TDB | jq -r '.tag_name | split(".")[1]')
 OLD_TDB_FOLDER=$OLD_TDB_VERSION'_'`date +%Y_%m_%d`
 OLD_TDB_URL=$( echo $OLD_TDB | jq -r '.assets | map(select(.name|endswith("7z"))) | .[0].browser_download_url')
-wget $OLD_TDB_URL -O TDB.7z
+wget $OLD_TDB_URL -q -O TDB.7z
 7z e TDB.7z
 
 #  8. set new TDB name and sql name
-NEW_TDB_VERSION=`date +%y%m0`
+NEW_TDB_VERSION=`date +%y%m1`
+if [ $NEW_TDB_VERSION == $OLD_TDB_VERSION ]; then
+  NEW_TDB_VERSION=$((NEW_TDB_VERSION + 1))
+fi
 TODAY=`date +%Y_%m_%d`
 NEW_TDB_TAG='TDB335.'$NEW_TDB_VERSION
 NEW_TDB_NAME='TDB 335.'$NEW_TDB_VERSION
 NEW_TDB_FILE='TDB_full_world_335.'$NEW_TDB_VERSION'_'$TODAY
+NEW_TDB_RELEASE_NOTES='Release '${NEW_TDB_VERSION: -1}' of '`date +%Y/%m`
 
 #  9. move all sql update scripts to old
-cd ../../../..
+cd ../../..
 mkdir sql/old/3.3.5a/auth/$OLD_TDB_FOLDER && mv sql/updates/auth/3.3.5/* sql/old/3.3.5a/auth/$OLD_TDB_FOLDER/
 mkdir sql/old/3.3.5a/characters/$OLD_TDB_FOLDER && mv sql/updates/characters/3.3.5/* sql/old/3.3.5a/characters/$OLD_TDB_FOLDER/
 mkdir sql/old/3.3.5a/world/$OLD_TDB_FOLDER && mv sql/updates/world/3.3.5/* sql/old/3.3.5a/world/$OLD_TDB_FOLDER/
@@ -76,7 +81,7 @@ do
     continue
   else
     cat >sql/updates/auth/3.3.5/$TODAY\_$(printf "%02d" $counter)_auth.sql <<EOL
--- TDB 335.64 auth
+-- $NEW_TDB_NAME auth
 UPDATE \`updates\` SET \`state\`='ARCHIVED';
 EOL
     break
@@ -89,7 +94,7 @@ do
     continue
   else
     cat >sql/updates/characters/3.3.5/$TODAY\_$(printf "%02d" $counter)_characters.sql <<EOL
--- TDB 335.64 characters
+-- $NEW_TDB_NAME characters
 UPDATE \`updates\` SET \`state\`='ARCHIVED';
 EOL
     break
@@ -102,7 +107,7 @@ do
     continue
   else
     cat >sql/updates/world/3.3.5/$TODAY\_$(printf "%02d" $counter)_world.sql <<EOL
--- TDB 335.64 world
+-- $NEW_TDB_NAME world
 UPDATE \`version\` SET \`db_version\`='$NEW_TDB_NAME', \`cache_id\`=$NEW_TDB_VERSION LIMIT 1;
 UPDATE \`updates\` SET \`state\`='ARCHIVED';
 EOL
@@ -113,7 +118,7 @@ git add sql
 
 # 11. let worldserver do the initial import and apply all updates
 cd bin/check_install/bin
-./worldserver
+./worldserver --update-databases-only
 cd ../../..
 
 # 12. update db setting ARCHIVED
@@ -150,7 +155,8 @@ git push $PUSH_URL >/dev/null 2>&1
 git tag $NEW_TDB_TAG
 git push $PUSH_URL $NEW_TDB_TAG >/dev/null 2>&1
 # 19. create a GitHub release
-NEW_RELEASE_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d '{"tag_name":"'"$NEW_TDB_TAG"'","target_commitish":"3.3.5","name":"'"$NEW_TDB_NAME"'","body":"### ![3.3.5](https://img.shields.io/badge/branch-3.3.5-yellow.svg)\n","draft":true,"prerelease":false}' $GITHUB_API)
-echo $NEW_RELEASE_RESPONSE | jq
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/octet-stream" $(echo $NEW_RELEASE_RESPONSE | jq -r '.upload_url' | sed -e 's${?name,label}$$g')'?name='"$NEW_TDB_FILE"'.7z'  --data-binary @$NEW_TDB_FILE.7z | jq
-curl -s -H "Authorization: token $GITHUB_TOKEN" -X 'PATCH' $(echo $NEW_RELEASE_RESPONSE | jq -r '.url') -d '{"draft":false}' | jq
+cd tdb
+NEW_RELEASE_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d '{"tag_name":"'"$NEW_TDB_TAG"'","target_commitish":"3.3.5","name":"'"$NEW_TDB_NAME"'","body":"### ![3.3.5](https://img.shields.io/badge/branch-3.3.5-yellow.svg)\n'"$NEW_TDB_RELEASE_NOTES"'\n","draft":true,"prerelease":false}' $GITHUB_API)
+echo $NEW_RELEASE_RESPONSE
+curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/octet-stream" $(echo $NEW_RELEASE_RESPONSE | jq -r '.upload_url' | sed -e 's${?name,label}$$g')'?name='"$NEW_TDB_FILE"'.7z'  --data-binary @$NEW_TDB_FILE.7z
+curl -s -H "Authorization: token $GITHUB_TOKEN" -X 'PATCH' $(echo $NEW_RELEASE_RESPONSE | jq -r '.url') -d '{"draft":false}'
