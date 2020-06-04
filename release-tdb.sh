8f0b79018e96d7b772f29bd879f84362d1489249
funjoker <funjoker109@gmail.com>
2020-06-04 21:22:41 +0200

    Port to master (8.3.0)

--- release-tdb.sh
+++ release-tdb.sh
@@ -6,7 +6,7 @@
 
 #  1. environmental variables
 #GITHUB_TOKEN=
-REPO_OWNER=TrinityCore
+REPO_OWNER=funjoker
 REPO_URL=https://github.com/${REPO_OWNER}/TrinityCore.git
 PUSH_URL=https://${GITHUB_TOKEN}@github.com/${REPO_OWNER}/TrinityCore.git
 GITHUB_API=https://api.github.com/repos/${REPO_OWNER}/TrinityCore/releases
@@ -14,8 +14,8 @@ GITHUB_API=https://api.github.com/repos/${REPO_OWNER}/TrinityCore/releases
 #  2. stop if any error happen
 set -e
 
-#  3. clone the repo
-git clone --branch=3.3.5 $REPO_URL server
+##  3. clone the repo
+git clone --branch=master $REPO_URL server
 cd server
 git config user.email "tdb-release@build.bot" && git config user.name "TDB Release"
 git status
@@ -25,11 +25,13 @@ mysql -uroot -e 'create database test_mysql;'
 mysql -uroot < sql/create/create_mysql.sql
 chmod +x contrib/check_updates.sh
 mysql -utrinity -ptrinity auth < sql/base/auth_database.sql
-./contrib/check_updates.sh auth 3.3.5 auth localhost
+./contrib/check_updates.sh auth master auth localhost
 mysql -utrinity -ptrinity characters < sql/base/characters_database.sql
-./contrib/check_updates.sh characters 3.3.5 characters localhost
+./contrib/check_updates.sh characters master characters localhost
 mysql -utrinity -ptrinity world < sql/base/dev/world_database.sql
-cat sql/updates/world/3.3.5/*.sql | mysql -utrinity -ptrinity world
+mysql -utrinity -ptrinity hotfixes < sql/base/dev/hotfixes_database.sql
+cat sql/updates/world/master/*.sql | mysql -utrinity -ptrinity world
+cat sql/updates/hotfixes/master/*.sql | mysql -utrinity -ptrinity hotfixes
 mysql -uroot < sql/create/drop_mysql.sql
 
 #  5. re-create the db to be used later
@@ -42,81 +44,114 @@ cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1
 $CXX --version
 make -j 4 -k && make install
 cd check_install/bin
-./authserver --version
+./bnetserver --version
 ./worldserver --version
 cp ../etc/worldserver.conf.dist ../etc/worldserver.conf
 
 #  7. download latest TDB
 OLD_TDB_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/TrinityCore/TrinityCore/releases)
-OLD_TDB=$(echo $OLD_TDB_RESPONSE | jq 'map(select(.tag_name|startswith("TDB335"))) | sort_by(.created_at) | reverse | .[0]')
+OLD_TDB=$(echo $OLD_TDB_RESPONSE | jq 'map(select(.tag_name|startswith("TDB335")|not)) | sort_by(.created_at) | reverse | .[0]')
+ARCHIVE_FOLDER=$(echo $OLD_TDB | jq -r '.tag_name | split(".")[0] | split("TDB")[1]' | sed 's/..$//')
 OLD_TDB_VERSION=$(echo $OLD_TDB | jq -r '.tag_name | split(".")[1]')
 OLD_TDB_FOLDER=$OLD_TDB_VERSION'_'`date +%Y_%m_%d`
 OLD_TDB_URL=$( echo $OLD_TDB | jq -r '.assets | map(select(.name|endswith("7z"))) | .[0].browser_download_url')
 wget $OLD_TDB_URL -q -O TDB.7z
 7z e TDB.7z
 
-#  8. set new TDB name and sql name
+# 8. let worldserver do the initial import and apply all updates
+./worldserver --update-databases-only
+cd ../../..
+
+#  9. set new TDB name and sql name
+MAJOR_VERSION=$(mysql -uroot auth -s -N -e "SELECT \`majorVersion\` FROM \`build_info\` ORDER BY \`build\` DESC LIMIT 0, 1;")
+MINOR_VERSION=$(mysql -uroot auth -s -N -e "SELECT \`minorVersion\` FROM \`build_info\` ORDER BY \`build\` DESC LIMIT 0, 1;")
+BUGFIX_VERSION=$(mysql -uroot auth -s -N -e "SELECT \`bugfixVersion\` FROM \`build_info\` ORDER BY \`build\` DESC LIMIT 0, 1;")
 NEW_TDB_VERSION=`date +%y%m1`
 if [ $NEW_TDB_VERSION == $OLD_TDB_VERSION ]; then
   NEW_TDB_VERSION=$((NEW_TDB_VERSION + 1))
 fi
 TODAY=`date +%Y_%m_%d`
-NEW_TDB_TAG='TDB335.'$NEW_TDB_VERSION
-NEW_TDB_NAME='TDB 335.'$NEW_TDB_VERSION
-NEW_TDB_FILE='TDB_full_world_335.'$NEW_TDB_VERSION'_'$TODAY
+NEW_WOW_PATCH="$MAJOR_VERSION$MINOR_VERSION$BUGFIX_VERSION"
+NEW_TDB_TAG='TDB'$NEW_WOW_PATCH'.'$NEW_TDB_VERSION
+NEW_TDB_NAME='TDB '$NEW_WOW_PATCH'.'$NEW_TDB_VERSION
+NEW_TDB_FILE='TDB_full_'$NEW_WOW_PATCH'.'$NEW_TDB_VERSION'_'$TODAY
+NEW_TDB_WORLD_FILE='TDB_full_world_'$NEW_WOW_PATCH'.'$NEW_TDB_VERSION'_'$TODAY
+NEW_TDB_HOTFIXES_FILE='TDB_full_hotfixes_'$NEW_WOW_PATCH'.'$NEW_TDB_VERSION'_'$TODAY
 NEW_TDB_RELEASE_NOTES='Release '${NEW_TDB_VERSION: -1}' of '`date +%Y/%m`
 
-#  9. move all sql update scripts to old
-cd ../../..
-mkdir sql/old/3.3.5a/auth/$OLD_TDB_FOLDER && mv sql/updates/auth/3.3.5/* sql/old/3.3.5a/auth/$OLD_TDB_FOLDER/
-mkdir sql/old/3.3.5a/characters/$OLD_TDB_FOLDER && mv sql/updates/characters/3.3.5/* sql/old/3.3.5a/characters/$OLD_TDB_FOLDER/
-mkdir sql/old/3.3.5a/world/$OLD_TDB_FOLDER && mv sql/updates/world/3.3.5/* sql/old/3.3.5a/world/$OLD_TDB_FOLDER/
-git add sql
+# 10. move all sql (excluding needed TDB Release sqls)  update scripts to old
+mkdir -p sql/old/$ARCHIVE_FOLDER.x/auth/$OLD_TDB_FOLDER && mv sql/updates/auth/master/* sql/old/$ARCHIVE_FOLDER.x/auth/$OLD_TDB_FOLDER/
+mkdir -p sql/old/$ARCHIVE_FOLDER.x/characters/$OLD_TDB_FOLDER && mv sql/updates/characters/master/* sql/old/$ARCHIVE_FOLDER.x/characters/$OLD_TDB_FOLDER/
+mkdir -p sql/old/$ARCHIVE_FOLDER.x/world/$OLD_TDB_FOLDER && mv sql/updates/world/master/* sql/old/$ARCHIVE_FOLDER.x/world/$OLD_TDB_FOLDER/
+mkdir -p sql/old/$ARCHIVE_FOLDER.x/hotfixes/$OLD_TDB_FOLDER && mv sql/updates/hotfixes/master/* sql/old/$ARCHIVE_FOLDER.x/hotfixes/$OLD_TDB_FOLDER/
+# 10.1 update db updates_include
+mysql -uroot -D auth -e "REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$ARCHIVE_FOLDER.x/auth', 'ARCHIVED');"
+mysql -uroot -D characters -e "REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$ARCHIVE_FOLDER.x/characters', 'ARCHIVED');"
+mysql -uroot -D world -e "REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$ARCHIVE_FOLDER.x/world', 'ARCHIVED');"
+mysql -uroot -D hotfixes -e "REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$ARCHIVE_FOLDER.x/hotfixes', 'ARCHIVED');"
 
-# 10. add the first sql update (making sure there isn't already a SQL file with the same name)
-# 10.1 auth db
+# 11. add the first sql update (making sure there isn't already a SQL file with the same name)
+# 11.1 auth db
 for((counter=0;;counter++))
 do
-  if [ -f "./sql/old/3.3.5a/auth/$OLD_TDB_FOLDER/"$TODAY"_$(printf "%02d" $counter)_auth.sql" ]; then
+  if test -n "$(find ./sql/old/"$ARCHIVE_FOLDER".x/auth/$OLD_TDB_FOLDER/ -maxdepth 1 -name "$TODAY"_$(printf "%02d" $counter)\*.sql -print -quit)"; then
     continue
   else
-    cat >sql/updates/auth/3.3.5/$TODAY\_$(printf "%02d" $counter)_auth.sql <<EOL
+    cat > sql/updates/auth/master/$TODAY\_$(printf "%02d" $counter)_auth.sql <<EOL
 -- $NEW_TDB_NAME auth
-UPDATE \`updates\` SET \`state\`='ARCHIVED';
+UPDATE \`updates\` SET \`state\`='ARCHIVED',\`speed\`=0;
+REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$MAJOR_VERSION.x/auth', 'ARCHIVED');
 EOL
     break
   fi
 done
-# 10.2 characters db
+# 11.2 characters db
 for((counter=0;;counter++))
 do
-  if [ -f "./sql/old/3.3.5a/characters/$OLD_TDB_FOLDER/"$TODAY"_$(printf "%02d" $counter)_characters.sql" ]; then
+  if test -n "$(find ./sql/old/"$ARCHIVE_FOLDER".x/characters/$OLD_TDB_FOLDER/ -maxdepth 1 -name "$TODAY"_$(printf "%02d" $counter)\*.sql -print -quit)"; then
     continue
   else
-    cat >sql/updates/characters/3.3.5/$TODAY\_$(printf "%02d" $counter)_characters.sql <<EOL
+    cat > sql/updates/characters/master/$TODAY\_$(printf "%02d" $counter)_characters.sql <<EOL
 -- $NEW_TDB_NAME characters
-UPDATE \`updates\` SET \`state\`='ARCHIVED';
+UPDATE \`updates\` SET \`state\`='ARCHIVED',\`speed\`=0;
+REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$MAJOR_VERSION.x/characters', 'ARCHIVED');
 EOL
     break
   fi
 done
-# 10.3 world db
+# 11.3 world db
 for((counter=0;;counter++))
 do
-  if [ -f "./sql/old/3.3.5a/world/$OLD_TDB_FOLDER/"$TODAY"_$(printf "%02d" $counter)_world.sql" ] || [ -f "./sql/old/3.3.5a/world/$OLD_TDB_FOLDER/"$TODAY"_$(printf "%02d" $counter)_world_335.sql" ] ; then
+  if test -n "$(find ./sql/old/"$ARCHIVE_FOLDER".x/world/$OLD_TDB_FOLDER/ -maxdepth 1 -name "$TODAY"_$(printf "%02d" $counter)\*.sql -print -quit)"; then
     continue
   else
-    cat >sql/updates/world/3.3.5/$TODAY\_$(printf "%02d" $counter)_world.sql <<EOL
+    cat > sql/updates/world/master/$TODAY\_$(printf "%02d" $counter)_world.sql <<EOL
 -- $NEW_TDB_NAME world
 UPDATE \`version\` SET \`db_version\`='$NEW_TDB_NAME', \`cache_id\`=$NEW_TDB_VERSION LIMIT 1;
-UPDATE \`updates\` SET \`state\`='ARCHIVED';
+UPDATE \`updates\` SET \`state\`='ARCHIVED',\`speed\`=0;
+REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$MAJOR_VERSION.x/world', 'ARCHIVED');
+EOL
+    break
+  fi
+done
+git add sql
+# 11.4 hotfixes db
+for((counter=0;;counter++))
+do
+  if test -n "$(find ./sql/old/"$ARCHIVE_FOLDER".x/hotfixes/$OLD_TDB_FOLDER/ -maxdepth 1 -name "$TODAY"_$(printf "%02d" $counter)\*.sql -print -quit)"; then
+    continue
+  else
+    cat > sql/updates/hotfixes/master/$TODAY\_$(printf "%02d" $counter)_hotfixes.sql <<EOL
+-- $NEW_TDB_NAME hotfixes
+UPDATE \`updates\` SET \`state\`='ARCHIVED',\`speed\`=0;
+REPLACE INTO \`updates_include\` (\`path\`, \`state\`) VALUES ('$/sql/old/$MAJOR_VERSION.x/hotfixes', 'ARCHIVED');
 EOL
     break
   fi
 done
 git add sql
 
-# 11. let worldserver do the initial import and apply all updates
+# 12. let worldserver add the fresh updates
 cd bin/check_install/bin
 ./worldserver --update-databases-only
 cd ../../..
@@ -125,6 +160,7 @@ cd ../../..
 mysql -uroot -D auth -e "update \`updates\` set \`state\`='ARCHIVED',\`speed\`=0;"
 mysql -uroot -D characters -e "update \`updates\` set \`state\`='ARCHIVED',\`speed\`=0;"
 mysql -uroot -D world -e "update \`updates\` set \`state\`='ARCHIVED',\`speed\`=0;"
+mysql -uroot -D hotfixes -e "update \`updates\` set \`state\`='ARCHIVED',\`speed\`=0;"
 # 12.b reset worldstates in characters db
 mysql -uroot -D characters -e "update \`worldstates\` set \`value\`=0;"
 
@@ -137,21 +173,25 @@ mysqldump -uroot characters --default-character-set='utf8' --result-file sql/bas
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/characters_database.sql
 sed -i -e 's$),($),\n($g' sql/base/characters_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/characters_database.sql
-mysqldump -uroot world --default-character-set='utf8' --no-data --result-file sql/base/dev/world_database.sql
+mysqldump -uroot world --default-character-set='utf8mb4' --no-data --result-file sql/base/dev/world_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/dev/world_database.sql
+mysqldump -uroot hotfixes --default-character-set='utf8mb4' --no-data --result-file sql/base/dev/hotfixes_database.sql
+sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/dev/hotfixes_database.sql
 git add sql
 
 # 14. dump world db to sql
 mkdir tdb
 cd tdb
-mysqldump -uroot world --default-character-set='utf8' --result-file $NEW_TDB_FILE.sql
+mysqldump -uroot world --default-character-set='utf8mb4' --result-file $NEW_TDB_WORLD_FILE.sql
+mysqldump -uroot hotfixes --default-character-set='utf8mb4' --result-file $NEW_TDB_HOTFIXES_FILE.sql
 
 # 15. 7zip the world db sql file
-7z a $NEW_TDB_FILE.7z $NEW_TDB_FILE.sql
+7z a $NEW_TDB_FILE.7z $NEW_TDB_WORLD_FILE.sql $NEW_TDB_HOTFIXES_FILE.sql
 
 # 16. update revision_data.h.in.cmake with new TDB file name
 cd ..
-sed -i -e 's$ #define _FULL_DATABASE             "[A-Za-z0-9$_.]*"$ #define _FULL_DATABASE             "'$NEW_TDB_FILE'.sql"$g' revision_data.h.in.cmake
+sed -i -e 's$ #define _FULL_DATABASE             "[A-Za-z0-9$_.]*"$ #define _FULL_DATABASE             "'$NEW_TDB_WORLD_FILE'.sql"$g' revision_data.h.in.cmake
+sed -i -e 's$ #define _HOTFIXES_DATABASE         "[A-Za-z0-9$_.]*"$ #define _HOTFIXES_DATABASE         "'$NEW_TDB_HOTFIXES_FILE'.sql"$g' revision_data.h.in.cmake
 git add revision_data.h.in.cmake
 # 17. commit and push
 git commit -m "$NEW_TDB_NAME - "`date +%Y/%m/%d`
@@ -161,7 +201,7 @@ git tag $NEW_TDB_TAG
 git push $PUSH_URL $NEW_TDB_TAG >/dev/null 2>&1
 # 19. create a GitHub release
 cd tdb
-NEW_RELEASE_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d '{"tag_name":"'"$NEW_TDB_TAG"'","target_commitish":"3.3.5","name":"'"$NEW_TDB_NAME"'","body":"### ![3.3.5](https://img.shields.io/badge/branch-3.3.5-yellow.svg)\n'"$NEW_TDB_RELEASE_NOTES"'\n","draft":true,"prerelease":false}' $GITHUB_API)
+NEW_RELEASE_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d '{"tag_name":"'"$NEW_TDB_TAG"'","target_commitish":"master","name":"'"$NEW_TDB_NAME"'","body":"### ![master](https://img.shields.io/badge/branch-master-yellow.svg)\n'"$NEW_TDB_RELEASE_NOTES"'\n","draft":true,"prerelease":false}' $GITHUB_API)
 echo $NEW_RELEASE_RESPONSE
 curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/octet-stream" $(echo $NEW_RELEASE_RESPONSE | jq -r '.upload_url' | sed -e 's${?name,label}$$g')'?name='"$NEW_TDB_FILE"'.7z'  --data-binary @$NEW_TDB_FILE.7z
 curl -s -H "Authorization: token $GITHUB_TOKEN" -X 'PATCH' $(echo $NEW_RELEASE_RESPONSE | jq -r '.url') -d '{"draft":false}'
