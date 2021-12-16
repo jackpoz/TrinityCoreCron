4c56d805db843089bba1bd0aa864088391b1ace1
jackpoz <giacomopoz@gmail.com>
2021-12-16 21:11:45 +0100

    Upgrade TDB release to MySQL 8

--- release-tdb.sh
+++ release-tdb.sh
@@ -21,6 +21,7 @@ git config user.email "tdb-release@build.bot" && git config user.name "TDB Relea
 git status
 
 #  4. setup the test db and check sql updates
+mysql -uroot -proot -e "SET PASSWORD FOR root@localhost='';"
 mysql -uroot -e 'create database test_mysql;'
 mysql -uroot < sql/create/create_mysql.sql
 chmod +x contrib/check_updates.sh
@@ -30,7 +31,7 @@ mysql -utrinity -ptrinity characters < sql/base/characters_database.sql
 ./contrib/check_updates.sh characters 3.3.5 characters localhost
 mysql -utrinity -ptrinity world < sql/base/dev/world_database.sql
 cat sql/updates/world/3.3.5/*.sql | mysql -utrinity -ptrinity world
-mysql -uroot < sql/create/drop_mysql.sql
+mysql -uroot < sql/create/drop_mysql_8.sql
 
 #  5. re-create the db to be used later
 mysql -uroot < sql/create/create_mysql.sql
@@ -133,12 +134,15 @@ mysqldump -uroot auth --default-character-set='utf8' --routines --result-file sq
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/auth_database.sql
 sed -i -e 's$),($),\n($g' sql/base/auth_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/auth_database.sql
+sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 mysqldump -uroot characters --default-character-set='utf8' --routines --result-file sql/base/characters_database.sql
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/characters_database.sql
 sed -i -e 's$),($),\n($g' sql/base/characters_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/characters_database.sql
+sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 mysqldump -uroot world --default-character-set='utf8' --routines --no-data --result-file sql/base/dev/world_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/dev/world_database.sql
+sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 git add sql
 
 # 14. dump world db to sql
@@ -152,7 +156,7 @@ sed -i -e 's/DEFINER=[^*]*\*/\*/' $NEW_TDB_FILE.sql
 
 # 16. recreate the dbs to test sql base files import
 cd ..
-mysql -uroot < sql/create/drop_mysql.sql
+mysql -uroot < sql/create/drop_mysql_8.sql
 mysql -uroot < sql/create/create_mysql.sql
 
 # 17. test sql base files import
@@ -161,7 +165,7 @@ mysql -uroot -D characters < sql/base/characters_database.sql
 mysql -uroot -D world < sql/base/dev/world_database.sql
 
 # 18. recreate the dbs to test TDB import
-mysql -uroot < sql/create/drop_mysql.sql
+mysql -uroot < sql/create/drop_mysql_8.sql
 mysql -uroot < sql/create/create_mysql.sql
 
 # 19. test TDB import
